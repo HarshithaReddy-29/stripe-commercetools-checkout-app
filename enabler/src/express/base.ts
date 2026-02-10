64bc4490fa29f700b9830acc9b413ce12073ff3f
HarshithaReddy-29 <harshithareddy.gangi@hcltech.com>
2026-02-09 21:48:02 -0600

    Update base.ts

--- enabler/src/express/base.ts
+++ enabler/src/express/base.ts
@@ -1,78 +1,46 @@
-import {
-  ExpressAddressData,
-  ExpressComponent,
-  ExpressOptions,
-  ExpressShippingOptionData,
-  OnComplete,
-} from "../payment-enabler/payment-enabler";
- 
-export abstract class DefaultExpressComponent implements ExpressComponent {
-  protected processorUrl: string;
-  protected sessionId: string;
-  protected countryCode: string;
-  protected currencyCode: string;
-  protected expressOptions: ExpressOptions;
-  protected availableShippingMethods: ExpressShippingOptionData[];
-  protected paymentMethodConfig: { [key: string]: string };
-  protected onComplete: OnComplete;
- 
-  constructor(opts: {
-    expressOptions: ExpressOptions;
-    processorUrl: string;
-    sessionId: string;
-    countryCode: string;
-    currencyCode: string;
-    paymentMethodConfig: { [key: string]: string };
-    onComplete: OnComplete;
-  }) {
-    this.expressOptions = opts.expressOptions;
-    this.processorUrl = opts.processorUrl;
-    this.sessionId = opts.sessionId;
-    this.countryCode = opts.countryCode;
-    this.currencyCode = opts.currencyCode;
-    this.paymentMethodConfig = opts.paymentMethodConfig;
-    this.onComplete = opts.onComplete;
+import { ComponentOptions, PaymentComponent, PaymentMethod } from '../payment-enabler/payment-enabler';
+import { BaseOptions } from "../payment-enabler/payment-enabler-mock";
+import {Stripe, StripePaymentElement} from "@stripe/stripe-js";
+import type { FakeSdk } from '../fake-sdk';
+
+
+
+/**
+ * Base Web Component
+ */
+export abstract class BaseComponent implements PaymentComponent {
+
+  protected paymentMethod: PaymentMethod;
+  protected processorUrl: BaseOptions['processorUrl'];
+  protected sessionId: BaseOptions['sessionId'];
+  protected environment: BaseOptions['environment'];
+  protected sdk: Stripe | FakeSdk;
+  protected stripePaymentElement: StripePaymentElement;
+
+  constructor(paymentMethod: PaymentMethod, baseOptions: BaseOptions, _componentOptions: ComponentOptions) {
+    this.paymentMethod = paymentMethod;
+    this.sdk = baseOptions.sdk;
+    this.processorUrl = baseOptions.processorUrl;
+    this.sessionId = baseOptions.sessionId;
+    this.environment = baseOptions.environment;
+    this.stripePaymentElement = baseOptions.paymentElement;
+
+    /**this.onComplete = baseOptions.configuration.onComplete;
+    this.onError = baseOptions.configuration.onError;**/
   }
- 
-  abstract init(): void;
- 
-  abstract mount(selector: string): void;
- 
-  async setShippingAddress(opts: {
-    address: ExpressAddressData;
-  }): Promise<void> {
-    if (this.expressOptions.onShippingAddressSelected) {
-      await this.expressOptions.onShippingAddressSelected(opts);
-      return;
+
+  abstract submit(): void;
+
+  abstract mount(selector: string): void ;
+
+  showValidation?(): void;
+  isValid?(): boolean;
+  getState?(): {
+    card?: {
+      endDigits?: string;
+      brand?: string;
+      expiryDate? : string;
     }
- 
-    throw new Error("setShippingAddress not implemented");
-  }
- 
-  async getShippingMethods(opts: {
-    address: ExpressAddressData;
-  }): Promise<ExpressShippingOptionData[]> {
-    if (this.expressOptions.getShippingMethods) {
-      this.availableShippingMethods =
-        await this.expressOptions.getShippingMethods(opts);
-      return this.availableShippingMethods;
-    }
- 
-    throw new Error("getShippingMethods not implemented");
-  }
- 
-  async setShippingMethod(opts: {
-    shippingMethod: { id: string };
-  }): Promise<void> {
-    if (this.expressOptions.onShippingMethodSelected) {
-      await this.expressOptions.onShippingMethodSelected(opts);
-      return;
-    }
- 
-    throw new Error("setShippingMethod not implemented");
-  }
- 
-  protected setSessionId(sessionId): void {
-    this.sessionId = sessionId;
-  }
+  };
+  isAvailable?(): Promise<boolean>;
 }
