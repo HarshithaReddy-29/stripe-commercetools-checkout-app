5bdea290f442d57317bb42dc7011d585b95ec37a
Harshitha Reddy Gangireddy <harshithareddy@Harshithas-MacBook-Air.local>
2026-02-17 11:11:35 -0600

    FIx

--- enabler/dev-utils/session.js
+++ enabler/dev-utils/session.js
@@ -1,16 +1,16 @@
-const projectKey = CTP_PROJECT_KEY;
+const projectKey = process.env.CTP_PROJECT_KEY;
 
 const fetchAdminToken = async () => {
   const myHeaders = new Headers();
 
-  myHeaders.append('Authorization', `Basic ${btoa(`${CTP_CLIENT_ID}:${CTP_CLIENT_SECRET}`)}`);
+  myHeaders.append('Authorization', `Basic ${btoa(`${process.env.CTP_CLIENT_ID}:${process.env.CTP_CLIENT_SECRET}`)}`);
   myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
 
   var urlencoded = new URLSearchParams();
   urlencoded.append('grant_type', 'client_credentials');
   //urlencoded.append('scope', __VITE_ADMIN_SCOPE__);
 
-  const response = await fetch(`${CTP_AUTH_URL}/oauth/token`, {
+  const response = await fetch(`${process.env.CTP_AUTH_URL}/oauth/token`, {
     body: urlencoded,
     headers: myHeaders,
     method: 'POST',
@@ -34,7 +34,7 @@ const getSessionId = async(cartId) => {
     allowedPaymentMethods: ["card", "invoice", "purchaseorder", "dropin","applepay","googlepay"], // add here your allowed methods for development purposes
   };
 
-  const url = `${CTP_SESSION_URL}/${projectKey}/sessions`
+  const url = `${process.env.CTP_SESSION_URL}/${projectKey}/sessions`
 
   const res = await fetch(url, {
     method: 'POST',
